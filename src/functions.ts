7ac3bc7f54e2d028b1e8d823d38c0588e0a5db83
DylanAdekpe <dylan.adekpe@modelis-tech.com>
2024-02-09 09:40:18 +0000

    init project

---
+++ src/functions.ts
@@ -0,0 +1,6 @@
+//Ce fichier permet d'ajouter toutes les functions qu'on aura utiliser dans notre module
+
+export function letsTest(name: string) {
+  console.log("We are testing if the name will display correctly");
+  console.log(`Your name is ${name}`);
+}
---
+++ src/types.ts
@@ -0,0 +1,1 @@
+// ce fichier permet de definir tout les types qu'on utilisera dans le modules
